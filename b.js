9d863f87269ed28b272932a5f1435e08fa07c750
black0tus <57235815+black0tus@users.noreply.github.com>
2019-10-31 22:39:50 +0200

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,2 @@
-document.getElementsByTagName("html")[0].innerHTML="<body style='background: black; text-align: center; color: green;'><img src='https://i.imgur.com/KSwlFiZ.png'/></br>Black Otus was here.</body>";
+var b = "<body style='background: black; text-align: center; color: green;'><div</body><div style='height: 100%;position: relative;'><div style='width: 100%; position: absolute;top: 50%;-ms-transform: translateY(-50%);transform: translateY(-50%);'><img style='height: 150' src='https://i.imgur.com/KSwlFiZ.png'/></br>black0tus was here.</div></div></body>"
+document.getElementsByTagName("html")[0].innerHTML= b;
