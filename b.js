8df9998b7a90f89885995700af4dea62b962af6e
black0tus <57235815+black0tus@users.noreply.github.com>
2019-10-31 22:12:15 +0200

    Add files via upload

---
+++ b.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName("body")[0].style='background: black; text-align: center; color: green;';
+document.getElementsByTagName("body")[0].innerHTML='<img src="https://i.imgur.com/KSwlFiZ.png"/></br>Black Otus was here.';
