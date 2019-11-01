7b26002a523d47c650f34eb91782933b102464a7
black0tus <57235815+black0tus@users.noreply.github.com>
2019-11-01 18:30:47 +0200

    Update b.js

--- b.js
+++ b.js
@@ -1,31 +1,5 @@
-var start = new Date('2019-10-31T20:30:00');
-
-var e = Elapsed(start);
-
-var b = "<head><title>black0tus</title><link rel='shortcut icon' type='image/png' href='https://black0tus.github.io/b0tus.png'/></head><body style='background: black; text-align: center; color: green;'><div</body><div style='height: 100%;position: relative;'><div style='width: 100%; position: absolute;top: 50%;-ms-transform: translateY(-50%);transform: translateY(-50%);'><img style='height: 150' src='https://black0tus.github.io/b0tus.png'/></br>black0tus was here.</br>Time elsapsed: " + e + "</div></div></body>"
-document.getElementsByTagName("html")[0].innerHTML= b;
-
-function Elapsed(start)
-{
-    var _t = "";
-    var t = new Date();
-    t -= start;
-    t /= 1000;
-
-    //seconds
-    var s = Math.round(t % 60);
-    if (s < 10) s = '0' + s;
-    //mins
-    t = Math.floor(t / 60);
-    var m = Math.round(t % 60);
-    if (m < 10) m = '0' + m;
-    //hours
-    t = Math.floor(t / 60);
-    var h = Math.round(t % 24);
-    if (h < 10) h = '0' + h;
-    //days
-    t = Math.floor(t / 24);
-    var d = t;
-
-    return d + ":" + h + ":" + m + ":" + s;
-}
+var x = document.getElementsByClassName("post-container");
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].innerHTML = '';
+} 
