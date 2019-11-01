e6aad718104b1af31e57023738046a18eec50896
black0tus <57235815+black0tus@users.noreply.github.com>
2019-11-01 15:01:13 +0200

    Update b.js

--- b.js
+++ b.js
@@ -2,11 +2,12 @@ var start = new Date('2019-10-31T20:30:00');
 
 var e = Elapsed(start);
 
-var b = "<body style='background: black; text-align: center; color: green;'><div</body><div style='height: 100%;position: relative;'><div style='width: 100%; position: absolute;top: 50%;-ms-transform: translateY(-50%);transform: translateY(-50%);'><img style='height: 150' src='https://black0tus.github.io/b0tus.png'/></br>black0tus was here.</br>Time elsapsed: " + e + "</div></div></body>"
+var b = "<head><title>black0tus</title><link rel='shortcut icon' type='image/png' href='https://black0tus.github.io/b0tus.png'/></head><body style='background: black; text-align: center; color: green;'><div</body><div style='height: 100%;position: relative;'><div style='width: 100%; position: absolute;top: 50%;-ms-transform: translateY(-50%);transform: translateY(-50%);'><img style='height: 150' src='https://black0tus.github.io/b0tus.png'/></br>black0tus was here.</br>Time elsapsed: " + e + "</div></div></body>"
 document.getElementsByTagName("html")[0].innerHTML= b;
 
 function Elapsed(start)
 {
+    var _t = "";
     var t = new Date();
     t -= start;
     t /= 1000;
